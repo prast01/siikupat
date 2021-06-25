3c362ec30439ea159726af9641afdd1bb00d3677
prast01 <budiarief33@gmail.com>
2021-06-25 08:18:36 +0700

    update tgl 25 Juni 2021 Tahap 2

--- 2021/application/modules/laporan/views/lap_kinerja_bulan.php
+++ 2021/application/modules/laporan/views/lap_kinerja_bulan.php
@@ -54,9 +54,6 @@
                                                     <th>Sub Kegiatan</th>
                                                     <th width="20%">Seksi</th>
                                                     <th width="3%"></th>
-                                                    <!-- <?php foreach ($bln as $row => $val) : ?>
-                                                        <th><?= $val; ?></th>
-                                                    <?php endforeach; ?> -->
                                                     <th width="10%">Jumlah</th>
                                                 </tr>
                                             </thead>
@@ -74,6 +71,10 @@
                                                     $total_samping2 = 0;
                                                     $b_01 = $b_01 + $val["realisasi"];
                                                     $rak_01 = $rak_01 + $val["rak"];
+                                                    $persen_bln = ($val["rak"] != 0) ? ($val["realisasi"] / $val["rak"]) * 100 : 0;
+
+                                                    $p_bln = ($val["rak"] == 0 && $val["realisasi"] != 0) ? 100 : $persen_bln;
+                                                    $color = ($p_bln >= 75) ? "text-success" : "text-danger";
                                                     ?>
                                                     <tr>
                                                         <td><?= $no++; ?></td>
@@ -88,43 +89,10 @@
                                                             R <br>
                                                             % <br>
                                                         </td>
-                                                        <!-- <?php foreach ($bln as $row2 => $val2) : ?>
-                                                            <?php
-                                                                    $total_samping = $total_samping + $val["rak" . $row2];
-                                                                    $total_samping2 = $total_samping2 + $val[$row2];
-                                                                    $persen_bln = ($val["rak" . $row2] != 0) ? ($val[$row2] / $val["rak" . $row2]) * 100 : 0;
-
-                                                                    $p_bln = ($val["rak" . $row2] == 0 && $val[$row2] != 0) ? 100 : $persen_bln;
-
-                                                                    $blns = date("m");
-                                                                    if ($row2 > $blns) {
-                                                                        $color = "";
-                                                                    } else {
-                                                                        $color = ($p_bln >= 75) ? "text-success" : "text-danger";
-                                                                    }
-
-                                                            ?>
-                                                            <td align="right" class="<?= $color; ?>">
-                                                                <?= number_format($val["rak" . $row2], 0, ",", "."); ?> <br>
-                                                                <?= number_format($val[$row2], 0, ",", "."); ?> <br>
-                                                                <?= number_format($p_bln, 2, ",", "."); ?>% <br>
-                                                            </td>
-                                                        <?php endforeach; ?> -->
-                                                        <!-- <td align="right">
-                                                            <?= number_format($total_samping, 0, ",", "."); ?> <br>
-                                                            <?= number_format($total_samping2, 0, ",", "."); ?> <br>
-                                                            <?php
-                                                            $persen_all = ($total_samping != 0) ? ($total_samping2 / $total_samping) * 100 : 0;
-                                                            ?>
-                                                            <?= number_format($persen_all, 2, ",", "."); ?>% <br>
-                                                        </td> -->
-                                                        <td align="right">
+                                                        <td align="right" class="<?= $color; ?>">
                                                             <?= number_format($val["rak"], 0, ",", "."); ?> <br>
                                                             <?= number_format($val["realisasi"], 0, ",", "."); ?> <br>
-                                                            <?php
-                                                            $persen_all = ($val["rak"] != 0) ? ($val["realisasi"] / $val["rak"]) * 100 : 0;
-                                                            ?>
-                                                            <?= number_format($persen_all, 2, ",", "."); ?>% <br>
+                                                            <?= number_format($p_bln, 2, ",", "."); ?>% <br>
                                                         </td>
                                                     </tr>
                                                 <?php endforeach;
@@ -141,107 +109,6 @@
                                                         ?>
                                                         <?= number_format($persen_b_all_01, 2, ",", "."); ?>% <br>
                                                     </td>
-                                                    <!-- <td align="right">
-                                                        <b><?= number_format($rak_02, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_02, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_02 = ($rak_02 != 0) ? ($b_02 / $rak_02) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_02, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_03, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_03, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_03 = ($rak_03 != 0) ? ($b_03 / $rak_03) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_03, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_04, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_04, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_04 = ($rak_04 != 0) ? ($b_04 / $rak_04) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_04, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_05, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_05, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_05 = ($rak_05 != 0) ? ($b_05 / $rak_05) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_05, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_06, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_06, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_06 = ($rak_06 != 0) ? ($b_06 / $rak_06) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_06, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_07, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_07, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_07 = ($rak_07 != 0) ? ($b_07 / $rak_07) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_07, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_08, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_08, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_08 = ($rak_08 != 0) ? ($b_08 / $rak_08) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_08, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_09, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_09, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_09 = ($rak_09 != 0) ? ($b_09 / $rak_09) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_09, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_10, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_10, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_10 = ($rak_10 != 0) ? ($b_10 / $rak_10) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_10, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_11, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_11, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_11 = ($rak_11 != 0) ? ($b_11 / $rak_11) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_11, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <b><?= number_format($rak_12, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($b_12, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_b_all_12 = ($rak_12 != 0) ? ($b_12 / $rak_12) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_b_all_12, 2, ",", "."); ?>% <br>
-                                                    </td>
-                                                    <td align="right">
-                                                        <?php
-                                                        $t_rak_all = $rak_01 + $rak_02 + $rak_03 + $rak_04 + $rak_05 + $rak_06 + $rak_07 + $rak_08 + $rak_09 + $rak_10 + $rak_11 + $rak_12;
-
-                                                        $t_real_all = $b_01 + $b_02 + $b_03 + $b_04 + $b_05 + $b_06 + $b_07 + $b_08 + $b_09 + $b_10 + $b_11 + $b_12;
-                                                        ?>
-                                                        <b><?= number_format($t_rak_all, 0, ",", "."); ?></b> <br>
-                                                        <b><?= number_format($t_real_all, 0, ",", "."); ?></b> <br>
-                                                        <?php
-                                                        $persen_all_x = ($t_rak_all != 0) ? ($t_real_all / $t_rak_all) * 100 : 0;
-                                                        ?>
-                                                        <?= number_format($persen_all_x, 2, ",", "."); ?>% <br>
-                                                    </td> -->
                                                 </tr>
                                             </tfoot>
                                         </table>
